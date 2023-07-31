8ee2bf796bc2fcbca03ed4f0801e79cec610de36
kaushikbarman449 <kaushikbarman449@gmail.com>
2023-07-31 15:32:20 +0530

    unnecesary components removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,15 @@
 import AboutSectionOne from "@/components/About/AboutSectionOne";
 import AboutSectionTwo from "@/components/About/AboutSectionTwo";
-import Blog from "@/components/Blog";
-import Brands from "@/components/Brands";
 import ChooseUs from "@/components/ChooseUs";
 import ScrollUp from "@/components/Common/ScrollUp";
-import Contact from "@/components/Contact";
 import Countries from "@/components/Countries";
 import FAQs from "@/components/FAQs";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
-import Pricing from "@/components/Pricing";
 import Projects from "@/components/Projects";
 import Testimonials from "@/components/Testimonials";
 import Video from "@/components/Video";
-import { Inter } from "@next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -26,15 +20,11 @@ export default function Home() {
       <Features />
       <AboutSectionTwo />
       <Video />
-      {/* <Brands /> */}
       <Countries />
       <ChooseUs />
       <Testimonials />
       <Projects />
       <FAQs />
-      {/* <Pricing /> */}
-      {/* <Blog /> */}
-      {/* <Contact /> */}
     </>
   );
 }
