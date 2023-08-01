38a539855eccfdc0c3cc10d2285a22c29640f84d
kaushikbarman449 <kaushikbarman449@gmail.com>
2023-08-01 16:19:06 +0530

    relevant svg icons added

--- components/ChooseUs/SingleChooseUs.tsx
+++ components/ChooseUs/SingleChooseUs.tsx
@@ -5,7 +5,7 @@ const SingleChooseUs = ({ feature }: { feature: Feature }) => {
     return (
         <div className="w-full">
             <div className="wow fadeInUp flex flex-col justify-center items-center" data-wow-delay=".15s">
-                <div className="mb-10 flex h-[70px] w-[70px] justify-center items-center rounded-md bg-primary bg-opacity-10 text-primary">
+                <div className="mb-10 flex h-[100px] w-[100px] justify-center items-center rounded-md bg-primary bg-opacity-10 text-primary">
                     {icon}
                 </div>
                 <h3 className="mb-5 text-xl text-center font-bold text-black dark:text-white sm:text-2xl lg:text-xl">
